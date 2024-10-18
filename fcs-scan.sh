38de5dfa10bb30abb4149ab17f0770e816b3c702
Carlos Matos <carlos.matos@crowdstrike.com>
2024-10-18 16:54:23 -0400

    test3

--- fcs-scan.sh
+++ fcs-scan.sh
@@ -145,7 +145,7 @@ execute_fcs_cli() {
     local fcs_image="${FCS_IMAGE}"
     [[ -n "$fcs_image" ]] || die "OUTPUT_FCS_IMAGE is not set. Ensure the FCS CLI container image was pulled successfully."
 
-    useradd -g 999 -u 999 cs
+    useradd -U -u 999 cs
     setfacl -m u:999:rwx "$GITHUB_WORKSPACE" || die "Failed to set permissions for container user."
     cd "$GITHUB_WORKSPACE" || die "Failed to change directory to $GITHUB_WORKSPACE"
 
